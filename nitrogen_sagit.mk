df80fd7d2006dec9eb57f096d56fddd2bd628059
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-04 15:17:58 +0300

    sagit: Implement libinit
    * Use pixel fingerprint to bypass safetynet

--- nitrogen_sagit.mk
+++ nitrogen_sagit.mk
@@ -31,11 +31,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys
-
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1080
 
